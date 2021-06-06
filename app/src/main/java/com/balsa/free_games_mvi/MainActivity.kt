dc12a74d4884349c0873bb690e09f3bb2db82e30
bakero98 <mugosab@gmail.com>
2021-06-06 15:26:09 +0200

    Initial commit

---
+++ app/src/main/java/com/balsa/free_games_mvi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.balsa.free_games_mvi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
